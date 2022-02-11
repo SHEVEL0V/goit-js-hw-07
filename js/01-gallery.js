9579c0e9e5d649a4c2ead81421343cfc5c5b4587
SHEVEL0V <ashevelevmti@gmail.com>
2022-02-11 11:04:26 +0200

    add gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.insertAdjacentHTML("beforeend", addItemGallery());
+
+function addItemGallery() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+              <a class="gallery__link" href="large-image.jpg">
+              <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="large-image.jpg"
+              alt="Image ${description}"
+              />
+              </a>
+              </div>`;
+    })
+    .join("");
+}
+
+// import * as basicLightbox from "basiclightbox";
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `);
+
+// instance.show();
